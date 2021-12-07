3f09809c88a2289a22c32a78ca8a733ac907d53c
mtxadmin <45539811+mtxadmin@users.noreply.github.com>
2021-12-07 18:32:52 +0300

    Create run_sql_script.ps1

---
+++ MSSQL/run_sql_script.ps1
@@ -0,0 +1,14 @@
+param( [string]$scriptpath )
+$prm = $args[3]
+if ($scriptpath -and $scriptpath.Length -gt 0) {
+    $scriptpath = "C:\zabbix\sql\$scriptpath"
+    $nc         = "C:\zabbix\sql\setnocount.sql"
+    if (Test-Path $scriptpath) {
+        if ($prm) {
+            $result = sqlcmd -m 1 -h -1 -W -v param=$prm -i $nc,$scriptpath
+        } else {
+            $result = sqlcmd -m 1 -h -1 -W -i $nc,$scriptpath
+        }
+    }
+    Write-Output $result
+}
